81f5c7f9c736b22ab662462982a720d1be9d9088
anhhuynhthe <122040050+anhhuynhthe@users.noreply.github.com>
2023-12-19 11:48:41 +0700

    thêm scene menu, đổi samplescene thành scene1

---
+++ Assets/Script/GameController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
